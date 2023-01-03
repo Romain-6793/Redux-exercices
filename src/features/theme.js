f59a18bc7bd28efb2f7bc0df22bed08d5d5af452
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2023-01-03 10:49:10 +0100

    theme createReducer set

--- src/features/theme.js
+++ src/features/theme.js
@@ -1,5 +1,8 @@
 
-import { createAction } from "@reduxjs/toolkit"
+//ATTENTION : La présence de currentTheme a modifié l'exemple par rapport au cours.
+
+
+import { createAction, createReducer } from "@reduxjs/toolkit"
 
 // On utilise des variables pour les noms des actions
 // pour éviter les fautes de frappe
@@ -18,14 +21,18 @@ export const setTheme = createAction('theme/set')
 
 // Le reducer
 // on utilise une valeur par défaut pour donner le state initial
-export default function reducer(state = initState, action) {
-    if (action.type === toggleTheme.toString()) {
-        const newTheme = state.currentTheme === 'light' ? 'dark' : 'light'
-        return { ...state, currentTheme: newTheme }
-    }
-    if (action.type === setTheme.toString()) {
-        const newState = { ...state, currentTheme: action.payload };
-        return newState;
-    }
-    return state
-}
+
+export default createReducer(initState, (builder) => {
+    return builder
+        .addCase(toggleTheme, (state) => {
+            const newTheme = state.currentTheme === 'light' ? 'dark' : 'light'
+            return { ...state, currentTheme: newTheme }
+        })
+        .addCase(setTheme, (state, action) => {
+            const newState = { ...state, currentTheme: action.payload };
+            return newState;
+        })
+
+})
+
+
